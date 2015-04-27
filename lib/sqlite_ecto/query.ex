30abe6cec69bbb5be94d30aeb3474e73152989ee
jazzyb <json.barnes@gmail.com>
2015-04-26 21:26:23 -0400

    cleans up alter table query code

--- lib/sqlite_ecto/query.ex
+++ lib/sqlite_ecto/query.ex
@@ -56,27 +56,59 @@ defmodule Sqlite.Ecto.Query do
 
   ## Helpers
 
-  # for algorithm see: http://www.sqlite.org/lang_altertable.html
+  # Sqlite does not implement the full ALTER TABLE statement.  It allows one
+  # to add columns and rename tables but not alter or drop columns.  This
+  # function implements the following generic algorithm that allows
+  # modification and removal of columns:
+  #
+  #   PRAGMA foreign_keys = OFF;
+  #   SAVEPOINT <savepoint>;
+  #   indices = (SELECT sql FROM sqlite_master WHERE tbl_name = <old> AND type = 'index';)
+  #   CREATE TABLE <new> ( ... new column definitions ... );
+  #   INSERT INTO <new> SELECT ... FROM <old>;
+  #   DROP TABLE <old>;
+  #   ALTER TABLE <new> RENAME TO <old>;
+  #
+  #   for column in add-changes:
+  #       -- add column to the table:
+  #       ALTER TABLE <old> ADD COLUMN ...;
+  #
+  #   for index in indices:
+  #       -- recreate index for table:
+  #       CREATE INDEX ...;
+  #
+  #   PRAGMA foreign_key_check; -- verify there are no violations
+  #   RELEASE <savepoint>
+  #   PRAGMA foreign_keys = ON;
+  #
+  # See: http://www.sqlite.org/lang_altertable.html
   defp alter_table_query(pid, alter) do
     {name, fields} = parse_alter_table_query(alter)
     with_foreign_keys_disabled(pid, fn ->
       with_savepoint(pid, fn ->
-        :ok = modify_table(pid, name, fields)
-        [] = Sqlitex.Server.query(pid, "PRAGMA foreign_key_check")
+        preserve_table_indices(pid, name, fn ->
+          :ok = modify_table(pid, name, fields)
+          [] = Sqlitex.Server.query(pid, "PRAGMA foreign_key_check")
+        end)
       end)
     end)
     {:ok, %{num_rows: 0, rows: []}}
   end
 
+  # Returns a tuple of the "unquoted" name of the table and lists of changes
+  # to be applied.
   defp parse_alter_table_query(sql) do
     name = parse_alter_table_name(sql)
     {name, parse_alter_table_fields(name, sql)}
   end
 
+  # Given a quoted table id, return the "unquoted" name.
   defp parse_alter_table_name("ALTER TABLE " <> rest) do
     rest |> String.split("\"") |> Enum.at(1)
   end
 
+  # Given the full alter table statement, return the changes that are to be
+  # applied to the table.
   defp parse_alter_table_fields(name, sql) do
     sql
     |> String.split("; ")
@@ -87,84 +119,96 @@ defmodule Sqlite.Ecto.Query do
     end)
   end
 
+  # Convert "ADD", "ALTER", and "DROP" column statements to tuples to make
+  # them easier to work with.
   defp to_field(<<"ADD COLUMN ", _::binary>> = stmt), do: {:add, stmt}
   defp to_field("ALTER COLUMN " <> field), do: {:modify, field}
   defp to_field("DROP COLUMN " <> col_name), do: {:remove, col_name}
 
+  # Turn off foreign key constraints for the duration of func/0.
   defp with_foreign_keys_disabled(pid, func) do
     :ok = exec(pid, "PRAGMA foreign_keys = OFF")
     func.()
     :ok = exec(pid, "PRAGMA foreign_keys = ON")
   end
 
-  defp modify_table(pid, name, changes) do
-    # save indices associated with the table
-    query = "SELECT sql FROM sqlite_master WHERE tbl_name = '#{name}' AND type = 'index'"
-    create_indices = Sqlitex.Server.query(pid, query) |> Enum.map(fn row -> row[:sql] end)
+  # Save the index schemas related to table 'name'.  Restore the saved indices
+  # after func/0 executes.
+  defp preserve_table_indices(pid, name, func) do
+    query = "SELECT sql FROM sqlite_master WHERE tbl_name = ?1 AND type = 'index'"
+    saved_indices = Sqlitex.Server.query(pid, query, bind: [name])
+    |> Enum.map(fn row -> row[:sql] end)
 
-    # split the fields for the original table columns
-    length = name |> quote_id |> bit_size
-    <<"CREATE TABLE ", _name::size(length), " (", fields::binary>> = table_schema(pid, name)
-    fields = fields |> String.rstrip(?)) |> String.split(", ")
+    func.()
 
-    # go through each of the fields applying drops and modifications
-    {column_names, fields} = fields
-    |> Enum.reduce({[], []}, fn (col, {names, fields}) ->
-      col_name = col |> String.split("\"") |> Enum.at(1) |> quote_id
-      case find_matching_change(col_name, changes) do
-        {:modify, new_field} ->
-          {[col_name | names], [new_field | fields]}
-        {:remove, _} ->
-          {names, fields}
-        _ ->
-          {[col_name | names], [col | fields]}
-      end
-    end)
-    |> (fn ({names, fields}) -> {Enum.reverse(names), Enum.reverse(fields)} end).()
+    Enum.each(saved_indices, fn sql -> :ok = exec(pid, sql) end)
+  end
 
-    # reconstruct the table schema with new fields and create a new table
+  # Modifies the columns in table 'old_tbl' based on 'changes'.  See comment
+  # above alter_table_query/3 for full algorithm for altering tables.
+  defp modify_table(pid, old_tbl, changes) do
+    {column_names, new_fields} = get_new_table_columns(pid, old_tbl, changes)
+    old_tbl = quote_id(old_tbl)
+
+    # create the new table
     new_tbl = "tbl_" <> random_id
-    create_tbl = "CREATE TABLE #{new_tbl} (#{Enum.join(fields, ", ")})"
+    create_tbl = "CREATE TABLE #{new_tbl} (#{Enum.join(new_fields, ", ")})"
     :ok = exec(pid, create_tbl)
 
-    # INSERT INTO new SELECT ... FROM name;
-    name = quote_id(name)
-    move_tbl = "INSERT INTO #{new_tbl} SELECT #{Enum.join(column_names, ",")} FROM #{name}"
+    # insert values from old table to new
+    move_tbl = "INSERT INTO #{new_tbl} SELECT #{Enum.join(column_names, ",")} FROM #{old_tbl}"
     :ok = exec(pid, move_tbl)
 
-    # DROP TABLE name;
-    :ok = exec(pid, "DROP TABLE #{name}")
+    # replace old table with new
+    :ok = exec(pid, "DROP TABLE #{old_tbl}")
+    :ok = exec(pid, "ALTER TABLE #{new_tbl} RENAME TO #{old_tbl}")
 
-    # ALTER TABLE new RENAME TO name;
-    :ok = exec(pid, "ALTER TABLE #{new_tbl} RENAME TO #{name}")
-
-    # add new columns
+    # add any columns new columns in 'changes'
     for {:add, col} <- changes do
-      :ok = exec(pid, "ALTER TABLE #{name} #{col}")
+      :ok = exec(pid, "ALTER TABLE #{old_tbl} #{col}")
     end
-
-    # restore saved indices
-    Enum.each(create_indices, fn sql -> :ok = exec(pid, sql) end)
-
     :ok
   end
 
+  # Return the new column names and column definitions given by 'changes'.
+  defp get_new_table_columns(pid, name, changes) do
+    create_tbl = table_schema(pid, name)
+    length = name |> quote_id |> bit_size
+    <<"CREATE TABLE ", _name::size(length), " (", column_defs::binary>> = create_tbl
+
+    column_defs
+    |> String.rstrip(?))
+    |> String.split(", ")
+       # take the column definitions and return the columns applying
+       # modifications and drops:
+    |> Enum.reduce({[], []}, fn (col, {names, defs}) ->
+      col_name = col |> String.split("\"") |> Enum.at(1) |> quote_id
+      case find_matching_change(col_name, changes) do
+        {:modify, new_field} ->
+          {[col_name | names], [new_field | defs]}
+        {:remove, _} ->
+          {names, defs}
+        _ ->
+          {[col_name | names], [col | defs]}
+      end
+    end)
+       # put them in the right order:
+    |> (fn ({names, defs}) -> {Enum.reverse(names), Enum.reverse(defs)} end).()
+  end
+
+  # Given a column name, find the change that is to be applied to that column
+  # (:modify or :remove).  Return the match or nil.
   defp find_matching_change(name, changes) do
     idx = Enum.find_index(changes, fn ({_action, stmt}) ->
       String.starts_with?(stmt, name)
     end)
-
-    if idx do
-      Enum.at(changes, idx)
-    else
-      nil
-    end
+    if idx, do: Enum.at(changes, idx), else: nil
   end
 
-  # find the schema for the table given by 'name'
+  # Find the schema for the table given by 'name'.
   defp table_schema(pid, name) do
-    query = "SELECT sql FROM sqlite_master WHERE name = '#{name}' AND type = 'table'"
-    [sql] = Sqlitex.Server.query(pid, query)
+    query = "SELECT sql FROM sqlite_master WHERE name = ?1 AND type = 'table'"
+    [sql] = Sqlitex.Server.query(pid, query, bind: [name])
     sql[:sql]
   end
 
