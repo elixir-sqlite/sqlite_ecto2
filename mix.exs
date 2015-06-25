e96007299ac63c90b7f535d067f0d303f1f5d60f
jazzyb <json.barnes@gmail.com>
2015-06-24 22:04:11 -0400

    update ecto dependency

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Sqlite.Ecto.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :ecto]]
   end
 
   # Dependencies can be Hex packages:
