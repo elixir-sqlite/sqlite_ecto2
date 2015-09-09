1dce0e90928875e752a927733a8067469c771241
jazzyb <json.barnes@gmail.com>
2015-09-09 18:11:10 -0400

    warn for unsupported constraints

--- lib/sqlite_ecto/error.ex
+++ lib/sqlite_ecto/error.ex
@@ -8,5 +8,15 @@ defmodule Sqlite.Ecto.Error do
   def exception({type, msg}), do: %__MODULE__{sqlite: {type, to_string(msg)}}
   def exception(msg), do: %__MODULE__{sqlite: msg}
 
+  require Logger
+
+  def to_constraints(%__MODULE__{sqlite: {:constraint, "UNIQUE constraint failed: " <> _}}) do
+    Logger.warn "unique_constraint/3 is unsupported by SQLite"
+    []
+  end
+  def to_constraints(%__MODULE__{sqlite: {:constraint, "FOREIGN KEY constraint failed"}}) do
+    Logger.warn "foreign_key_constraint/3 is unsupported by SQLite"
+    []
+  end
   def to_constraints(_), do: []
 end
