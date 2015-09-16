6912eefe4f540de89a2c488eb8b2116ae227d86d
jazzyb <json.barnes@gmail.com>
2015-09-16 13:31:10 -0400

    moves unsupported constraint message to exception

--- lib/sqlite_ecto/error.ex
+++ lib/sqlite_ecto/error.ex
@@ -5,18 +5,18 @@ defmodule Sqlite.Ecto.Error do
   def message(%__MODULE__{sqlite: msg}), do: "UNKNOWN: #{inspect msg}"
   def message(%__MODULE__{message: msg}), do: msg
 
+  # handle unique constraint failures
+  def exception({:constraint, msg = 'UNIQUE constraint failed:' ++ _}) do
+    msg = to_string(msg) <> "; unique_constraint/3 is unsupported by SQLite"
+    %__MODULE__{sqlite: {:constraint, msg}}
+  end
+  # handle foreign key constraint failures
+  def exception({:constraint, msg = 'FOREIGN KEY constraint failed'}) do
+    msg = to_string(msg) <> "; foreign_key_constraint/3 is unsupported by SQLite"
+    %__MODULE__{sqlite: {:constraint, msg}}
+  end
   def exception({type, msg}), do: %__MODULE__{sqlite: {type, to_string(msg)}}
   def exception(msg), do: %__MODULE__{sqlite: msg}
 
-  require Logger
-
-  def to_constraints(%__MODULE__{sqlite: {:constraint, "UNIQUE constraint failed: " <> _}}) do
-    Logger.warn "unique_constraint/3 is unsupported by SQLite"
-    []
-  end
-  def to_constraints(%__MODULE__{sqlite: {:constraint, "FOREIGN KEY constraint failed"}}) do
-    Logger.warn "foreign_key_constraint/3 is unsupported by SQLite"
-    []
-  end
   def to_constraints(_), do: []
 end
