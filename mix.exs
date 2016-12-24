32ad6309438aeb1f765ef3a6ffbd5f1adc4bfde2
Eric Scouten <eric@scouten.com>
2016-12-23 17:43:02 -0800

    We'll call it 2.0 even though it really isn't yet.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sqlite.Ecto.Mixfile do
 
   def project do
     [app: :sqlite_ecto2,
-     version: "2.0-alpha",
+     version: "2.0",
      name: "Sqlite.Ecto2",
      elixir: "~> 1.2",
      deps: deps,
