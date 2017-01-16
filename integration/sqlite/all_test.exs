905007c0740a3b009dab12d1f8c0722bdb79fadb
Eric Scouten <eric@scouten.com>
2017-01-16 12:09:07 -0800

    Add Elixir 1.4 to the text matrix. (#33)
    * Add Elixir 1.4 to the text matrix.
    
    Now that @tchoutri has fixed the existing 1.4 warnings (thank you!), let's keep it going.
    
    * Fix a few Elixir 1.4 bareword warnings.
    
    * When compiling old Ecto code with new Elixir (1.4.x), disable warnings-as-errors.
    
    * D'oh. Pattern match fail wasn't handled.

--- integration/sqlite/all_test.exs
+++ integration/sqlite/all_test.exs
@@ -1,3 +1,9 @@
+# Old Ecto files don't compile cleanly in Elixir 1.4, so we disable warnings first.
+case System.version() do
+  "1.4." <> _ -> Code.compiler_options(warnings_as_errors: false)
+  _ -> :ok
+end
+
 Code.require_file "../../deps/ecto/integration_test/cases/assoc.exs", __DIR__
 Code.require_file "../../deps/ecto/integration_test/cases/interval.exs", __DIR__
 Code.require_file "../../deps/ecto/integration_test/cases/joins.exs", __DIR__
