93c09214c1960d9f9b28efe8bf6403f6e5ee8a5f
Eric Scouten <eric@scouten.com>
2017-01-19 14:27:06 -0800

    Sqlite.DbConnection.Error can be ever so much simpler.

--- lib/sqlite_db_connection/error.ex
+++ lib/sqlite_db_connection/error.ex
@@ -1,25 +1,3 @@
 defmodule Sqlite.DbConnection.Error do
   defexception [:message, :sqlite, :postgres]
-
-  # TODO: Retrofit to actual SQLite error schema.
-
-  def message(e) do
-    if kw = e.postgres do
-      "#{kw[:severity]} (#{kw[:code]}): #{kw[:message]}"
-    else
-      e.message
-    end
-  end
-
-  def exception([postgres: fields]) do
-    fields = Enum.into(fields, %{})
-             |> Map.put(:pg_code, fields[:code])
-             |> Map.update!(:code, &Sqlite.DbConnection.ErrorCode.code_to_name/1)
-
-    %Sqlite.DbConnection.Error{postgres: fields}
-  end
-
-  def exception(arg) do
-    super(arg)
-  end
 end
