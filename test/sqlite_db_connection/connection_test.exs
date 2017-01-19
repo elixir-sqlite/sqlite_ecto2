82ed56a8c8d9cef4c03a0e3e9d02da8801f8862c
Eric Scouten <eric@scouten.com>
2017-01-19 15:12:38 -0800

    Add tests for Connection.prepare!.

--- test/sqlite_db_connection/connection_test.exs
+++ test/sqlite_db_connection/connection_test.exs
@@ -24,4 +24,26 @@ defmodule ConnectionTest do
     assert %Sqlite.DbConnection.Error{message: "near \"huh\": syntax error",
                                       sqlite: %{code: :sqlite_error}} = err
   end
+
+  test "prepare!", context do
+    pid = context[:pid]
+    stmt = P.prepare!(pid, "prepare_test", "SELECT 42", [])
+    assert %Sqlite.DbConnection.Query{prepared: sqlitex,
+                                      name: "prepare_test",
+                                      statement: "SELECT 42"} = stmt
+    assert %Sqlitex.Statement{column_names: [:"42"]} = sqlitex
+  end
+
+  test "prepare! failure case", context do
+    pid = context[:pid]
+    assert_raise Sqlite.DbConnection.Error, fn ->
+      P.prepare!(pid, "prepare_test", "huh", [])
+    end
+  end
+
+  test "execute", context do
+    pid = context[:pid]
+    assert {:ok, result} = P.execute(pid, "prepare_test", "SELECT 42", [])
+    IO.puts "execute result = #{inspect result}"
+  end
 end
