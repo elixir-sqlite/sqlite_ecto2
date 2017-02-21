cae70fc5590f6c9714398c448c503ea707348497
Eric Scouten <eric@scouten.com>
2017-02-20 16:54:14 -0800

    Advance to "Use DBConnection new log system" commit.

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Sqlite.Ecto.Mixfile do
      {:coverex, "~> 1.4.11", only: :test},
      {:db_connection, git: "https://github.com/elixir-ecto/db_connection", ref: "625e42521e2b72f597c388daa6ecf5a90d7bd6e7", override: true}, # version 0.1.7
      {:ex_doc, "~> 0.14.5", only: :dev},
-     {:ecto, git: "https://github.com/scouten/ecto.git", ref: "b738eb1967bac6c0ab1f56925a1ed19a6fb5413f"},
+     {:ecto, git: "https://github.com/scouten/ecto.git", ref: "8e2a9274fc5ab6b9b973ca50f2045c5b952617bf"},
      {:poison, "~> 1.0"},
      {:postgrex, git: "https://github.com/ericmj/postgrex.git", ref: "f773f8d4b88565aea435c6651b711ebfce74c9c2", override: true},
      {:sbroker, "~> 1.0", override: true},
