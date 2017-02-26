f4cbbe1a5e04ce44d072ceb0efa733ac75027ad0
Eric Scouten <eric@scouten.com>
2017-02-26 10:02:35 -0800

    Pick up fix for VM crash from esqlite. Thanks @mmzeeman!

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Sqlite.Ecto.Mixfile do
      {:connection, "~> 1.0.2", override: true},
      {:coverex, "~> 1.4.11", only: :test},
      {:db_connection, git: "https://github.com/fishcakez/db_connection", ref: "f438a1d66f238cc3d786f7aa7719fc36eaa283f2", override: true}, # version 0.1.7
-     {:esqlite, git: "https://github.com/mmzeeman/esqlite", ref: "785903ae6d157ebda824e4e841f1f63aa93f9208", override: true},
+     {:esqlite, git: "https://github.com/mmzeeman/esqlite", ref: "3f1ef40b9011276eb8bdc366c5ef1e25d79befa5", override: true},
      {:ex_doc, "~> 0.14.5", only: :dev},
      {:ecto, git: "https://github.com/scouten/ecto.git", ref: "8e2a9274fc5ab6b9b973ca50f2045c5b952617bf"},
      {:poison, "~> 1.0"},
