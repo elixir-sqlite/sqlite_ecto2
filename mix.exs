e445bb7968222dcfda3786b3808eed4e0e3e110a
Eric Scouten <eric@scouten.com>
2017-03-05 22:59:14 -0800

    Advance to "Add Repo.in_transaction?" commit from 26 Jan 2016.

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Sqlite.Ecto.Mixfile do
      {:decimal, "1.1.1", override: true},
      {:esqlite, git: "https://github.com/mmzeeman/esqlite", ref: "c1ba116de470aadc23e7ae582c961b2ced13d306", override: true},
      {:ex_doc, "~> 0.14.5", only: :dev},
-     {:ecto, git: "https://github.com/scouten/ecto.git", ref: "d405eb170f1f5b7e062091e7b598220ed7e57a20"},
+     {:ecto, git: "https://github.com/scouten/ecto.git", ref: "485d4e8cd516027c8b83fc34c93af45d545e3626"},
      {:poison, "1.5.2", override: true, optional: true},
      {:postgrex, "0.11.0", optional: true},
      {:sbroker, "~> 1.0", override: true},
