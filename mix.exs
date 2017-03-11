7eacb41751ba4f8c0de390fc6a350dfc3ee28f89
Eric Scouten <eric@scouten.com>
2017-03-11 13:13:25 -0800

    Advance to "Support returning in update_all and delete_all" commit. (#65)
    * Advance to "Support returning in update_all and delete_all" commit from 03 Feb 2016.
    
    * Update for latest changes.

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Sqlite.Ecto.Mixfile do
      {:decimal, "1.1.1", override: true},
      {:esqlite, git: "https://github.com/mmzeeman/esqlite", ref: "c1ba116de470aadc23e7ae582c961b2ced13d306", override: true},
      {:ex_doc, "~> 0.14.5", only: :dev},
-     {:ecto, git: "https://github.com/scouten/ecto.git", ref: "cbc54d7351dc7ccf47ab7c48e3b8a02bb17f3ea3"},
+     {:ecto, git: "https://github.com/scouten/ecto.git", ref: "8af2ec3ce3b1870aaa35eec49519e9038baba690"},
      {:poison, "1.5.2", override: true, optional: true},
      {:postgrex, "0.11.0", optional: true},
      {:sbroker, "~> 1.0", override: true},
