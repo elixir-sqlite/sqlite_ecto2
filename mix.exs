e82b2ac90a5059728326cf53101f4009491c8303
Eric Scouten <eric@scouten.com>
2017-03-11 16:40:22 -0800

    Catch up to Ecto 2.0.0-rc.1, released 19 Apr 2016. (#75)
    * Catch up to Ecto 2.0.0-rc.1, released 19 Apr 2016.
    
    * Pick up a test diff I had missed earlier.
    
    * Remove patches for integration test scripts that are no longer needed.

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Sqlite.Ecto.Mixfile do
      {:decimal, "1.1.1", override: true},
      {:esqlite, git: "https://github.com/mmzeeman/esqlite", ref: "c1ba116de470aadc23e7ae582c961b2ced13d306", override: true},
      {:ex_doc, "~> 0.14.5", only: :dev},
-     {:ecto, git: "https://github.com/scouten/ecto.git", ref: "66ae92fd61b3351c1bfbbb88a4804fce23d26f12"},
+     {:ecto, git: "https://github.com/scouten/ecto.git", ref: "dcb7256367c3c9e7d0272c824757d35f01dc548c"},
      {:poison, "1.5.2", override: true, optional: true},
      {:postgrex, "~> 0.11.1", optional: true},
      {:sbroker, "~> 1.0", override: true},
