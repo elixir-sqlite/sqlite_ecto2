14f823c77ad1f12ecb8f553642b0f3c113210c54
Eric Scouten <eric@scouten.com>
2017-03-11 16:37:28 -0800

    Remove patches for integration test scripts that are no longer needed.

--- integration/hack_out_incompatible_tests.sh
+++ integration/hack_out_incompatible_tests.sh
@@ -22,27 +22,3 @@ sed -i '/test "insert all/ i @tag :insert_cell_wise_defaults' deps/ecto/integrat
 sed -i '/test "Repo.insert_all escape/ i @tag :insert_cell_wise_defaults' deps/ecto/integration_test/sql/sql.exs
 
 fi
-
-# Backport this change until we catch up to that point in time (15 Feb 2016):
-# https://github.com/elixir-ecto/ecto/commit/235c099a7856eb4451ccfbaede249a59d20b0c66#diff-3079a35f77dacc7bdd7cc2e69c39a886
-if [ `uname` == "Darwin" ] ; then
-
-sed -i '' '62s/TestRepo/PoolRepo/' deps/ecto/integration_test/sql/transaction.exs
-
-else
-
-sed -i '62s/TestRepo/PoolRepo/' deps/ecto/integration_test/sql/transaction.exs
-
-fi
-
-# Backport this change until we catch up to that point in time (15 Feb 2016):
-# https://github.com/elixir-ecto/ecto/commit/235c099a7856eb4451ccfbaede249a59d20b0c66#diff-3079a35f77dacc7bdd7cc2e69c39a886
-if [ `uname` == "Darwin" ] ; then
-
-sed -i '' '468s/assert %Decimal{} = TestRepo.aggregate(Post, :avg, :visits)/assert "12.5" <> _ = to_string(TestRepo.aggregate(Post, :avg, :visits))/' deps/ecto/integration_test/cases/repo.exs
-
-else
-
-sed -i '468s/assert %Decimal{} = TestRepo.aggregate(Post, :avg, :visits)/assert "12.5" <> _ = to_string(TestRepo.aggregate(Post, :avg, :visits))/' deps/ecto/integration_test/cases/repo.exs
-
-fi
