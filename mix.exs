b20dcf4a3d7c42287c991d2b3df21749bd815a13
Eric Scouten <eric@scouten.com>
2017-03-13 08:46:42 -0700

    Catch up to Ecto 2.0.1, released 21 Jun 2016.

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Sqlite.Ecto.Mixfile do
      {:decimal, "1.1.2", override: true},
      {:esqlite, git: "https://github.com/mmzeeman/esqlite", ref: "c1ba116de470aadc23e7ae582c961b2ced13d306", override: true},
      {:ex_doc, "~> 0.14.5", only: :dev},
-     {:ecto, git: "https://github.com/elixir-ecto/ecto.git", ref: "c225f0ed89c85fa49b0b062fc51d82ae6bf17083"},
+     {:ecto, git: "https://github.com/elixir-ecto/ecto.git", ref: "7105a26fc72cbad83cdb81dd06dec01f33e41629"},
      {:poison, "1.5.2", override: true, optional: true},
      {:postgrex, git: "https://github.com/ericmj/postgrex", ref: "93bfef47b418162b9804c5ad952606214ec4b00e", override: true},
      {:sbroker, "~> 1.0", override: true},
