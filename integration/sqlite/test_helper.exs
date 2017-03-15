0d771f33defd095607b79afd726feb75016b3e2f
Eric Scouten <eric@scouten.com>
2017-03-14 20:16:03 -0700

    Update for API changes.

--- integration/sqlite/test_helper.exs
+++ integration/sqlite/test_helper.exs
@@ -18,7 +18,8 @@ ExUnit.start exclude: [:array_type,
                        :modify_foreign_key_on_update,
                        :alter_primary_key,
                        :map_boolean_in_subquery,
-                       :upsert]
+                       :upsert_with_conflict_target,
+                       :upsert_without_conflict_target]
 
 # Configure Ecto for support and tests
 Application.put_env(:ecto, :primary_key_type, :id)
