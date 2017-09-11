39a7de1663e6cb4739570550639622b1ea5d9281
Eric Scouten <eric@scouten.com>
2017-09-10 18:00:39 -0700

    Update references to point to Ecto 2.2.2.

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Sqlite.Ecto2.Mixfile do
      {:dogma, "~> 0.1", only: :dev},
      {:esqlite, "~> 0.2.3"},
      {:ex_doc, "~> 0.16", only: :dev},
-     {:ecto, "~> 2.2.0"},
+     {:ecto, "~> 2.2.2"},
      {:poison, "~> 2.2 or ~> 3.0", optional: true},
      {:postgrex, "~> 0.13.0", optional: true},
      {:sbroker, "~> 1.0"},
