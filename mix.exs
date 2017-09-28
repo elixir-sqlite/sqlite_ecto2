56f4e5daf1d8f7a85d480a2c51a42f3a923a2a95
Eric Scouten <eric@scouten.com>
2017-09-27 17:44:08 -0700

    Advance to "Fix typespecs for multi" commit from 20 Sep 2017.
    (And we're now caught up to current master branch.)

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Sqlite.Ecto2.Mixfile do
      {:decimal, "~> 1.5"},
      {:excoveralls, "~> 0.9", only: :test},
      {:ex_doc, "~> 0.18", runtime: false, only: :docs},
-     {:ecto, github: "elixir-ecto/ecto", ref: "d28aebac6c3b8203750bd23e965a7ba19d42bda6"},
+     {:ecto, github: "elixir-ecto/ecto", ref: "3a0616a356ecbc3b6ba1e391e785a13f66c13da9"},
      {:poison, "~> 2.2 or ~> 3.0", optional: true},
      {:postgrex, "~> 0.13", optional: true},
      {:sbroker, "~> 1.0"},
