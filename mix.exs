9b31d5e5bfd3da12cc037b6a2d75856a0e6b4846
Eric Scouten <eric@scouten.com>
2018-06-17 10:14:05 -0700

    Prepare 2.2.3 release. (#211)

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sqlite.Ecto2.Mixfile do
 
   def project do
     [app: :sqlite_ecto2,
-     version: "2.2.2",
+     version: "2.2.3",
      name: "Sqlite.Ecto2",
      elixir: "~> 1.4",
      elixirc_options: [warnings_as_errors: true],
